59832f17f162304e1b82e8688e7217afaebc7d76
Ernesto ARTIGAS <ernart99@gmail.com>
2025-10-22 23:55:38 +0200

    feat: Added route for triggering delete task

--- app/playback_sessions/job.py
+++ app/playback_sessions/job.py
@@ -9,7 +9,5 @@ def delete_inactive_playback_sessions() -> None:
         if not jellyfin_client.is_session_active(id=playback_session.id):
             playback_sessions_to_delete.append(playback_session.id)
 
-    print("playback", playback_sessions_to_delete)
-
     for playback_session_id in playback_sessions_to_delete:
         service.delete_playback_session(id=playback_session_id)
