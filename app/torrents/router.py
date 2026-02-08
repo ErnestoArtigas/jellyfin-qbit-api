218ba667b32b2abcb05dc8d0fc041c41d3c22f99
ErnestoArtigas <43345969+ErnestoArtigas@users.noreply.github.com>
2026-02-08 20:44:08 +0100

    feat: Added playback existence check for start torrent, refactored playback_session model to remove event listeners (#3)

--- app/torrents/router.py
+++ app/torrents/router.py
@@ -6,11 +6,11 @@ from . import service
 router = APIRouter()
 
 
-@router.get("/start", status_code=status.HTTP_204_NO_CONTENT)
-def start_torrents():
-    return service.start_torrents()
+@router.get("/start/", status_code=status.HTTP_204_NO_CONTENT)
+def start_torrents(playback_check: bool = False) -> None:
+    return service.start_torrents(playback_existence_check=playback_check)
 
 
-@router.get("/stop", status_code=status.HTTP_204_NO_CONTENT)
-def stop_torrents():
+@router.get("/stop/", status_code=status.HTTP_204_NO_CONTENT)
+def stop_torrents() -> None:
     return service.stop_torrents()
